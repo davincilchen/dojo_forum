7914b9956277fcc266811534bfd8cd0eb6cfbcb4
davincilchen <doki2099@gmail.com>
2018-04-21 16:52:48 +0800

    set up routes and empty VC for dojo (artical)

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -1,4 +1,29 @@
 class DojosController < ApplicationController
   def index
   end
+
+   def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+  end
+
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
