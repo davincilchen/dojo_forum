66211483de5dcb10e6ecb2909cafaa8b10e3bfb9
davincilchen <doki2099@gmail.com>
2018-04-23 17:06:58 +0800

    add user's edit view and action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,29 @@
 class UsersController < ApplicationController
-  before_action :set_user, only:[:show]
+  before_action :set_user, only:[:show, :edit, :update]
 
   def show
 
   end 
+
+  def edit
+    if @user != current_user
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    if @user == current_user
+      if @user.update(user_params)
+        flash[:notice] = "User was successfully update"
+        redirect_to edit_user_path
+      else
+        flash.now[:alert] = "User was failed to update"
+        render :edit
+      end
+    else
+      redirect_to user_path(@user)
+    end
+  end
 
   private
 
