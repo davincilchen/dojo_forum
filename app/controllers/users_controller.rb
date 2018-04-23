7103a5e20f5dcce014d24eb266213612bd2a168e
davincilchen <doki2099@gmail.com>
2018-04-23 16:45:09 +0800

    add some basic user's info in user's show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show]
 
   def show
 
   end 
+
+  private
+
+  def user_params
+     params.require(:user).permit(:name, :introduction ,:avatar)
+  end
+
+  def set_user
+    puts "1--0-0-qeaq"
+    @user = User.find(params[:id])
+  end
+
 end
