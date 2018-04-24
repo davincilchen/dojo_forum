970e928534805d83dafcbd321f52fa83f2709e9c
davincilchen <doki2099@gmail.com>
2018-04-24 17:20:04 +0800

    user can add comment on dojo (post) show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+
+
+  def create
+    @dojo = Dojo.find(params[:dojo_id])
+    @comment = @dojo.comments.build(comment_params)
+    @comment.user = current_user
+    @comment.save!
+    redirect_to dojo_path(@dojo)
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -22,6 +22,7 @@ class DojosController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
   end
 
 
