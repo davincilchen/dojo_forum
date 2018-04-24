35b0ded5314709cc48bbf7c1ec93ddb89268a127
davincilchen <doki2099@gmail.com>
2018-04-24 20:09:29 +0800

    user can delete comment on dojo page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,25 @@ class CommentsController < ApplicationController
   end
 
 
+
+  def destroy
+    @dojo = Dojo.find(params[:dojo_id])
+    @comment = Comment.find(params[:id])
+
+    if @comment
+      if @comment.user == current_user
+        @comment.destroy
+        flash[:alert] = "Comment was deleted"
+      else
+        flash[:alert] = "Current user is not user of the comment"
+      end
+    else
+      flash[:alert] = "Fail to delete comment"
+    end
+    redirect_to dojo_path(@dojo)
+  end
+
+
   private
 
   def comment_params
