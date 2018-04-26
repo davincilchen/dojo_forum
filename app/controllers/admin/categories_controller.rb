e595c711c1b6e121ce1ca3bc156665bbbb47cd76
davincilchen <doki2099@gmail.com>
2018-04-26 17:12:50 +0800

    admin can add new category and category can't be exist

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,4 +3,32 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+
+    if ! Category.where('lower(name) = ?', @category.name.downcase).first
+      if @category.save
+        redirect_to admin_categories_path, notice: "Category was successfully created"
+      else
+        @categories = Category.all
+        render :index
+      end
+    else
+      flash[:alert] = "Category was already exist"
+      @categories = Category.all
+      render :index
+    end
+  end
+
+ private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
 end
