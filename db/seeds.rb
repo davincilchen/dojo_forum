23a2d471cdff6b24e0bd93daa5eff1370db370ca
davincilchen <doki2099@gmail.com>
2018-04-26 15:54:28 +0800

    1.rails generate model category 2.add default category and admin in db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+# Category
+Category.destroy_all
+
+category_list = [
+  { name: "Sport" },
+  { name: "Art" },
+  { name: "Food" },
+  { name: "Book" },
+  { name: "Economic" },
+  { name: "Mood" },
+  { name: "Travel" }
+]
+
+
+category_list.each do |category|
+  Category.create( name: category[:name])
+end
+puts "create category (seeds)"
+
+
+
+#Default admin
+User.create(email: "admin@example.com", password: "12345678", role: "admin", name: "Admin")
+puts "Default admin created!"
