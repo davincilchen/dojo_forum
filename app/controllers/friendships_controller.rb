336122e33e6835d11ff02311f9da1c09883c91d9
davincilchen <doki2099@gmail.com>
2018-04-29 22:33:52 +0800

    user ajax to process friend action (create, accept and ignore)

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,19 +2,21 @@ class FriendshipsController < ApplicationController
   def create
     @user = User.find(params[:friend_id])
     current_user.create_friendships(@user)
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
   end
 
 
   def accept
     @friendship = current_user.inverse_friendships.find_by(user_id: params[:id])
     @friendship.update(accepted: true)
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
+    @user = User.find(params[:id]) #user.id for ajax
   end
 
   def ignore
     @friendship = current_user.inverse_friendships.find_by(user_id: params[:id])
     @friendship.destroy
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
+    @user = User.find(params[:id]) #user.id for ajax
   end
 end
